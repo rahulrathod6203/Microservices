3f4ee8e884c58f638a5fc4b7073f352595a05561
Rahul Rathod <rahulrathod6203@gmail.com>
2026-06-19 02:23:51 -0500

    Student entity created and Postgres database connection established

---
+++ student-service/src/main/java/com/cg/model/Student.java
@@ -0,0 +1,33 @@
+package com.cg.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "students")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "address_id", nullable = false)
+    private Long addressId;
+
+}
